e83f5c99584df05475326718d20bfee1ab916a35
eliotmess <olejniczak.grzes@gmail.com>
2019-04-15 21:21:44 +0200

    Change gitignore

---
+++ build/precache-manifest.6b4d11f48db10386e426b462f4c69da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a85b066015282890533f",
+    "url": "/static/js/main.8bac4315.chunk.js"
+  },
+  {
+    "revision": "c48cd2aabb2172f21932",
+    "url": "/static/js/2.08ccc77a.chunk.js"
+  },
+  {
+    "revision": "a85b066015282890533f",
+    "url": "/static/css/main.f70300ab.chunk.css"
+  },
+  {
+    "revision": "c48cd2aabb2172f21932",
+    "url": "/static/css/2.c2741336.chunk.css"
+  },
+  {
+    "revision": "6afc53584a2494209c647127b7bf6048",
+    "url": "/index.html"
+  }
+];
